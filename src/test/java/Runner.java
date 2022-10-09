f0767e538e0fb7bbb3dfa4c64bb1b84fce583fb0
Azohba <onurtarar@gmail.com>
2022-10-09 14:35:03 +0300

    calliope

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,6 +1,10 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import utils.Calliope;
+
+import java.io.IOException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -8,5 +12,11 @@ import org.junit.runner.RunWith;
         glue = {"steps"},
         plugin = {"pretty","junit:reports/cucumber-results.xml","json:reports/cucumber-results.json"})
 public class Runner {
+
+    @AfterClass
+    public static void sent() throws IOException{
+        Calliope calliope = new Calliope();
+        calliope.sendHTTPReq();
+    }
 
 }
---
+++ src/test/java/utils/Calliope.java
@@ -0,0 +1,27 @@
+package utils;
+
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Calliope {
+
+    public void sendHTTPReq() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("multipart/form-data");
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("file[]","/C:/ProgramData/Jenkins/.jenkins/workspace/SpriteCloudCucumberProject/reports/cucumber-results.json",
+                        RequestBody.create(MediaType.parse("application/octet-stream"),
+                                new File("/C:/ProgramData/Jenkins/.jenkins/workspace/SpriteCloudCucumberProject/reports/cucumber-results.json")))
+                .build();
+        Request request = new Request.Builder()
+                .url("https://app.calliope.pro/api/v2/profile/4508/import/")
+                .method("POST", body)
+                .addHeader("x-api-key", "MmU3MjM4YmJiZTdiOTRlMjQwOTlkZWMwYTJmNzExNDA5ZjQyYzA5MjRiNGRkNzY0NDU1MGRlYmJiYTkxZDljY2Jj")
+                .addHeader("Content-Type", "multipart/form-data")
+                .build();
+        Response response = client.newCall(request).execute();
+    }
+}
